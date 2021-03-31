ef4acc0d3d45cd73f980b1df8b3c11f924e11221
pranavagrao121 <81469461+pranavagrao121@users.noreply.github.com>
2021-03-31 19:46:56 +0530

    Add files via upload

---
+++ MoneyDivider.py
@@ -0,0 +1,7 @@
+#this is a simple app to divide the amount to the givern members
+
+money = int(input("enter the value"))
+members = int(input("enter the value"))
+shares = money // members
+
+print(shares)
